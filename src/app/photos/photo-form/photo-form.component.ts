d5763aaac24de01aa33b2d7c1cfb1825f6c12149
joasimonson <joasimonson@hotmail.com>
2021-03-27 01:13:22 -0300

    implementacão de diretiva para abrir seleção de imagem após ir para página de adicionar imagem

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
-import { PhotoModule } from '../photo/photo.module';
 
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
+import { PhotoModule } from '../photo/photo.module';
 import { PhotoFormComponent } from './photo-form.component';
 
 @NgModule({
@@ -15,7 +16,8 @@ import { PhotoFormComponent } from './photo-form.component';
     VMessageModule,
     FormsModule,
     RouterModule,
-    PhotoModule
+    PhotoModule,
+    ImmediateClickModule
   ],
 })
 export class PhotoFormModule {}
