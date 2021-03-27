a96c267c5e7b8eba34541d9b960bd0eae15533b2
joasimonson <joasimonson@hotmail.com>
2021-03-27 01:26:20 -0300

    criação de guard para autenticação, alteração de guard para login, utilização em rota para inclusão de foto

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './core/auth/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
@@ -26,6 +27,7 @@ const routes: Routes = [
   {
     path: 'p/add',
     component: PhotoFormComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: '**',
--- src/app/core/auth/login.guard.ts
+++ src/app/core/auth/login.guard.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
-import { UserService } from '../user/user.service';
 import {
-  CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
+  CanActivate,
   Router,
+  RouterStateSnapshot,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { UserService } from '../user/user.service';
+
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class LoginGuard implements CanActivate {
   constructor(private userService: UserService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | Observable<boolean> | Promise<boolean> {
-    if (!this.userService.isLogged()) {
-      this.router.navigate(['']);
+    if (this.userService.isLogged()) {
+      this.router.navigate(['user', this.userService.getUserName()])
       return false;
     }
     return true;
