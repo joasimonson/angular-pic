e85d4fe6a985366ffed0ed2068d1c6b8b3fa7c29
joasimonson <joasimonson@hotmail.com>
2021-03-29 01:32:55 -0300

    remoção de foto do history api após exclusão; tratamento para redirecionar usuário após login

--- web/src/app/home/signin/signin.component.ts
+++ web/src/app/home/signin/signin.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
@@ -10,17 +10,23 @@ import { PlatformDetectorService } from 'src/app/core/platform-detector/platform
   styleUrls: ['./signin.component.css'],
 })
 export class SignInComponent implements OnInit {
+  fromUrl: string;
   loginForm: FormGroup;
   @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService,
     private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService,
     private platformDetectorService: PlatformDetectorService
   ) {}
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(
+      (params) => (this.fromUrl = params['fromUrl'])
+    );
+
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
@@ -34,7 +40,10 @@ export class SignInComponent implements OnInit {
     const password = this.loginForm.get('password').value;
 
     this.authService.authenticate(userName, password).subscribe(
-      () => this.router.navigate(['user', userName]),
+      () =>
+        this.fromUrl
+          ? this.router.navigateByUrl(this.fromUrl)
+          : this.router.navigate(['user', userName]),
       (err) => {
         this.loginForm.reset();
         this.platformDetectorService.isPlatformBrowser() &&
--- web/src/app/photos/photo-details/photo-details.component.ts
+++ web/src/app/photos/photo-details/photo-details.component.ts
@@ -40,7 +40,7 @@ export class PhotoDetailsComponent implements OnInit {
     this.photoService.removePhoto(this.photoId).subscribe(
       () => {
         this.alertService.success('Photo removed', true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
+        this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
       },
       (err) => {
         console.log(err);
